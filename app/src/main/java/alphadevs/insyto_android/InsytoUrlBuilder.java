6daf1cbcfa41054ab6e157d70c50c9364ecac57d
Gorik <igor1991@live.ca>
2015-12-02 14:34:29 -0500

    add category and author to insyte list item

--- app/src/main/java/alphadevs/insyto_android/InsytoUrlBuilder.java
+++ app/src/main/java/alphadevs/insyto_android/InsytoUrlBuilder.java
@@ -6,7 +6,7 @@ import android.net.Uri;
  * Use it to create required URLs for insyto.
  */
 public class InsytoUrlBuilder {
-    private final static String BASE_URL = "http://f0b3a36c.ngrok.io"; // TODO change per config
+    private final static String BASE_URL = "http://36c50429.ngrok.io"; // TODO change per config
     private final static String API_V1 = "v1";
     private final static String INSYTES = "insytes";
     private final static String PAGE_PARAM = "page";
